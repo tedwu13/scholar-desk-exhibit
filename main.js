3356d9ce7cfd1df5765202ba78d75b52a0db8f32
Ted Wu <tedwu2016@u.northwestern.edu>
2016-05-18 15:29:41 -0500

    add hovering for inkstone

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var inkStoneTouched = false;
 
 $(function(){
-    $('#inkstone').click(function(e) {
+    $('#inkstone').click(function() {
         inkStoneTouched = true;
         if (inkStoneTouched) {
             $('#simple_sketch').sketch();
         }
     });
-
 });

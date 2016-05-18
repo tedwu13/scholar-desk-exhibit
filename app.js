7e8fb5193e734207c05f5dcecee79988446df117
Ted Wu <tedwu2016@u.northwestern.edu>
2016-05-18 18:40:59 -0500

    refactor into node app

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html' , { root : __dirname});
+})
+
+
+var server = app.listen(8000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})

0ca706291d4ff1d4a15f4775313d97b4e6e0bdab
Ted Wu <tedwu2016@u.northwestern.edu>
2016-05-18 18:46:03 -0500

    test deploy

--- app.js
+++ app.js
@@ -8,12 +8,11 @@ app.get('/', function (req, res) {
     res.sendFile('index.html' , { root : __dirname});
 })
 
-
 var server = app.listen(8000, function () {
 
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("App listening at http://%s:%s", host, port)
 
 })
